9185be4e527748bc9dc2382d8748218a6b2d7034
Abhinavkumar2025 <abhinavku04@gmail.com>
2026-02-16 16:16:42 +0530

    added mongo atlas

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/Manora');
-        console.log("MongoDB Connected");
+        const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/Manora');
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(error.message);
         process.exit(1);
